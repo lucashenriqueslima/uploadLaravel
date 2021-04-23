17170fb8027bc0476ef8133c7160efdae53d842d
lucashenriqueslima <lucashenriqueslima@outlook.com>
2021-04-23 12:00:00 -0300

    Primeiro Comit

---
+++ resources/views/events/show.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.main')
+
+@section('title','Criar Evento')
+
+@section('content')
+    
+    <div class="col-md10 offset-md-1"></div>
+        <div class="row">
+            <div id="image-container" class="col-md-6">
+                <img src="/img/events/{{ $event->image }}" class="img-fluid" alt="{{$event->title}}">
+            </div>
+            <div id="info-container" class="mol-md-6">
+                <h1> {{ $event->title}} </h1>
+                <p class="event-city"><ion-icon name="location-outline"></ion-icon>{{$event->city}}</p>
+                <p class="events-participants"><ion-icon name="people-outline"></ion-icon> X Paticipantes</p>
+                <p class="event-owner"><ion-icon name="star-outline"></ion-icon> Dono do Evento</p>
+                <a href="#" class="btn btn-primary" id="event-submit"> Confirmar Presença </a>
+            <div class="col-md-12" id="description-container">
+                <h3> Sobre o evento: </h3>
+                <p class="event-description"> {{$event->description}} </p>
+                
+            </div>
+        </div>
+    </div>
+
+
+@endsection
